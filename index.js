de6face3d55f1058c00cb379633c1554630dbb29
optimisticnpc <joshuajltan@gmail.com>
2024-12-28 10:47:28 +1300

    Middleware

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const http = require('http')
+const express = require('express')
+const app = express()
 
+app.use(express.json())
+
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
 
 let notes = [
   {
@@ -18,11 +32,63 @@ let notes = [
     important: true
   }
 ]
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' })
-  response.end(JSON.stringify(notes))
-})
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)s
+
+  app.get('/', (request, response) => {
+    response.send('<h1>Helladsfasdfo World!</h1>')
+  })
+  
+  app.get('/api/notes', (request, response) => {
+    response.json(notes)
+  })
+
+  app.get('/api/notes/:id', (request, response) => {
+    const id = request.params.id
+    const note = notes.find(note => note.id === id)
+    
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
+  })
+
+  const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => Number(n.id)))
+      : 0
+    return String(maxId + 1)
+  }
+  
+  app.post('/api/notes', (request, response) => {
+    const body = request.body
+  
+    if (!body.content) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+  
+    const note = {
+      content: body.content,
+      important: Boolean(body.important) || false,
+      id: generateId(),
+    }
+  
+    notes = notes.concat(note)
+  
+    response.json(note)
+  })
+
+
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  app.use(unknownEndpoint)
+  
+  const PORT = 3002
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+
