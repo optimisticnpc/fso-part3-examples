84840020555da027d494dfdb7186970775429bc4
optimisticnpc <joshuajltan@gmail.com>
2024-12-30 14:27:38 +1300

    Ready for deployment

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const http = require('http')
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
-
+app.use(cors())
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -87,7 +88,7 @@ let notes = [
   
   app.use(unknownEndpoint)
   
-  const PORT = 3002
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
