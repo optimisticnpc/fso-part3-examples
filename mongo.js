96d0d9b8d3c0e6af48e07d3420e7be2feedbaa07
optimisticnpc <joshuajltan@gmail.com>
2026-01-27 13:11:49 +1300

    DB Works Locally

---
+++ mongo.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+// const url = `mongodb+srv://fullstack:${password}@cluster0.vr2pg.mongodb.net/?appName=Cluster0`
+
+// mongodb+srv://<db_username>:<db_password>@cluster0.vr2pg.mongodb.net/?appName=Cluster0
+
+const url = `mongodb://localhost:27017/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'You did it!',
+  important: true,
+})
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
