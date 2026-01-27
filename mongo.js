b3a15c897561fc18463ab417689de36c5cf2bdf2
optimisitcnpc <joshuajltan@gmail.com>
2026-01-27 20:03:55 +1300

    mongo.js works

--- mongo.js
+++ mongo.js
@@ -7,11 +7,11 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-// const url = `mongodb+srv://fullstack:${password}@cluster0.vr2pg.mongodb.net/?appName=Cluster0`
+const url = `mongodb+srv://fullstack:${password}@cluster0.vr2pg.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
 
 // mongodb+srv://<db_username>:<db_password>@cluster0.vr2pg.mongodb.net/?appName=Cluster0
 
-const url = `mongodb://localhost:27017/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+// const url = `mongodb://localhost:27017/noteApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery',false)
 
@@ -24,12 +24,19 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-const note = new Note({
-  content: 'You did it!',
-  important: true,
-})
+// const note = new Note({
+//   content: 'weeeeeeeeeeeeeeee!',
+//   important: true,
+// })
 
-note.save().then(result => {
-  console.log('note saved!')
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
   mongoose.connection.close()
 })
