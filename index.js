a0568d719c6a2ae18445fec0fe711dc49570bde6
optimisticnpc <joshuajltan@gmail.com>
2026-01-28 16:13:23 +1300

    Delete notes and error handler

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 require('dotenv').config()
 const Note = require('./models/note')
 
-const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
-app.use(express.static('dist'))
+
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -21,7 +21,7 @@ const requestLogger = (request, response, next) => {
 app.use(requestLogger)
 
 app.get('/', (request, response) => {
-  response.send('<h1>Helladsfasdfo World!</h1>')
+  response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
@@ -30,12 +30,19 @@ Note.find({}).then(notes => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
-})
+// Get by ID
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
 
+    .catch(error => next(error))
+})
 // Create
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -78,12 +85,33 @@ app.put('/api/notes/:id', (request, response) => {
   response.json(updatedNote)
 })
 
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
--- mongo.js
+++ mongo.js
@@ -7,11 +7,11 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.vr2pg.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+// const url = `mongodb+srv://fullstack:${password}@cluster0.vr2pg.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
 
 // mongodb+srv://<db_username>:<db_password>@cluster0.vr2pg.mongodb.net/?appName=Cluster0
 
-// const url = `mongodb://localhost:27017/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb://localhost:27017/noteApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery',false)
 
